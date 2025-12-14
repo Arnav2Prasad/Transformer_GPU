916122f6311d399ebb43a497cf446250805f259f
Arnav Prasad <arnavprasad@Arnavs-MacBook-Pro.local>
2025-12-14 15:24:43 +0530

    ignore -- kaggle param change

--- WANDB/config_code.py
+++ WANDB/config_code.py
@@ -339,41 +339,6 @@ class Trainconfig:
 
 
 
-# ____________ UTIL FUNCTIONS _________________
-
-def get_lr(iter, TrainingConfig:Trainconfig):
-    max_lr = TrainingConfig.learning_rate
-    min_lr = max_lr*0.1
-    max_decay_steps = TrainingConfig.max_iters + 2 # avoid division by zero
-    # 1) linear warump for warmup_steps:
-    if iter < TrainingConfig.warmup_steps:
-        return max_lr * (iter+1)/TrainingConfig.warmup_steps
-    #2) if iter > lr_decay_iters, return min_lr
-    elif iter > max_decay_steps:
-        return min_lr
-    #3) in between, use cosine decay
-    else:
-        decay_ratio = (iter - TrainingConfig.warmup_steps) / (max_decay_steps - TrainingConfig.warmup_steps)
-        decay_ratio = min(decay_ratio, 1.0)  # ensure it does
-        coeff = 0.5 * (1 + math.cos(math.pi * decay_ratio))
-        return min_lr + coeff * (max_lr - min_lr)
-
-@torch.no_grad()
-def estimate_loss(model:LLM, TrainingConfig:Trainconfig, train_loader:DataLoader, val_loader:DataLoader):
-    out = {}
-    model.eval() ; model.VAL_RUN = True
-    for split, loader in [('train', train_loader), ('val', val_loader)]:
-        losses = torch.zeros(TrainingConfig.eval_iters)
-        for k in range(TrainingConfig.eval_iters):
-            X, Y = loader.next_batch() # Data is now moved to device in next_batch()
-            with ctx:
-                _, loss, _ = model(X, Y)
-            losses[k] = loss.item()
-        out[split] = losses.mean()
-    model.train(); model.VAL_RUN = False
-    return out
-
-
 
 
 
@@ -1474,3 +1439,41 @@ class MoE(nn.Module):
             return super().load_state_dict(state_dict, strict=strict)
 
  
+
+
+
+
+# ____________ UTIL FUNCTIONS _________________
+
+def get_lr(iter, TrainingConfig:Trainconfig):
+    max_lr = TrainingConfig.learning_rate
+    min_lr = max_lr*0.1
+    max_decay_steps = TrainingConfig.max_iters + 2 # avoid division by zero
+    # 1) linear warump for warmup_steps:
+    if iter < TrainingConfig.warmup_steps:
+        return max_lr * (iter+1)/TrainingConfig.warmup_steps
+    #2) if iter > lr_decay_iters, return min_lr
+    elif iter > max_decay_steps:
+        return min_lr
+    #3) in between, use cosine decay
+    else:
+        decay_ratio = (iter - TrainingConfig.warmup_steps) / (max_decay_steps - TrainingConfig.warmup_steps)
+        decay_ratio = min(decay_ratio, 1.0)  # ensure it does
+        coeff = 0.5 * (1 + math.cos(math.pi * decay_ratio))
+        return min_lr + coeff * (max_lr - min_lr)
+
+@torch.no_grad()
+def estimate_loss(model:LLM, TrainingConfig:Trainconfig, train_loader:DataLoader, val_loader:DataLoader):
+    out = {}
+    model.eval() ; model.VAL_RUN = True
+    for split, loader in [('train', train_loader), ('val', val_loader)]:
+        losses = torch.zeros(TrainingConfig.eval_iters)
+        for k in range(TrainingConfig.eval_iters):
+            X, Y = loader.next_batch() # Data is now moved to device in next_batch()
+            with ctx:
+                _, loss, _ = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train(); model.VAL_RUN = False
+    return out
+
