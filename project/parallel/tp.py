4eac367dff7512896c47af40efef13a87e0953fc
Arnav Prasad <arnavprasad@Arnavs-MacBook-Pro.local>
2026-01-03 00:34:06 +0530

    fix - modular code of all 1D parallelism techniques

--- project/parallel/tp.py
+++ project/parallel/tp.py
@@ -3,7 +3,7 @@
 import torch
 import torch.nn as nn
 import torch.distributed as dist
-
+import os
 
 class ColumnParallelLinear(nn.Module):
     """Shard the weight matrix along output dimension (column-wise)"""
