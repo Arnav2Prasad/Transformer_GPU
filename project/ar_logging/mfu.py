9cdac14559ce2de6b8cefec1f12493eaeadfdde9
Arnav2Prasad <arnav.prasad.ap@gmail.com>
2026-02-08 00:53:29 +0530

    fix - mfu func()

--- project/ar_logging/mfu.py
+++ project/ar_logging/mfu.py
@@ -364,7 +364,7 @@ def arnav_compute_mfu_from_configs(
 
     # Account for gradient accumulation
     # flops_per_iter = flops_per_fwdbwd * grad_accum_steps
-    flops_per_fwdbwd = grad_accum_steps * batch_size
+    fwdbwd_per_iter = grad_accum_steps * batch_size
     flops_per_iter = flops_per_fwdbwd * fwdbwd_per_iter
     print('flops_per_iter')
 
